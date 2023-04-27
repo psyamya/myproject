2d078c829f9cf54de0ca47db147a8196825c44ae
psyamya <121109706+psyamya@users.noreply.github.com>
2023-04-27 15:19:05 +0530

    Add files via upload

---
+++ file2.js
@@ -0,0 +1,22 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
+
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add('close');
+    menu.classList.add('show');
+    menuNav.classList.add('show');
+    menuBranding.classList.add('show');
+    navItems.forEach(item => item.classList.add('show'));
+
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove('close');
+  }
